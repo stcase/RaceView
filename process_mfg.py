48916c979cbd5638156ed8e9c346c8b144af7ab3
stcase <stcinc@gmail.com>
2017-10-02 19:22:27 -0700

    Add support for specifying a race name

--- process_mfg.py
+++ process_mfg.py
@@ -4,24 +4,30 @@ import os
 import sys
 from string import Template
 
-SCHEMA_FLAT = ["Category", "Place", "Bib", "Name", "Team name", "Age", "Year of birth", "Time"]
-SCHEMA_RESHAPE = ["Lap", "Lap time"]
-DEFAULT_CATEGORY = "Cat 1/2 Men"
-DEFAULT_VALUE = "-"
-
-IN_DELIM = "\t"
-OUT_DELIM = ","
 
 class Reshaper:
     """ Reshapes race results with lap data into a Tableau-readable form """
 
-    def __init__(self, inFoldName, outFileName):
+    def __init__(self, inFoldName, outFileName, raceName=""):
         self.__logger = logging.getLogger(__name__)  
         self.__inFoldName = inFoldName
         self.__outFileName = outFileName
         
-        self.__NUM_EXPECTED_FILES = 7
         self.__rawFiles = []
+        
+        self.__NUM_EXPECTED_FILES = 7
+        self.__SCHEMA_FLAT = ["Category", "Place", "Bib", "Name", "Team name", "Age", "Year of birth", "Time"]
+        self.__SCHEMA_RESHAPE = ["Lap", "Lap time"]
+        self.__DEFAULT_VALUE = "-"
+        self.__IN_DELIM = "\t"
+        self.__OUT_DELIM = ","
+        self.__DEFAULT_VALUES = {}
+        for col in self.__SCHEMA_FLAT:
+            self.__DEFAULT_VALUES[col] = self.__DEFAULT_VALUE
+        self.__DEFAULT_VALUES["Category"] = "Cat 1/2 Men"
+        if  raceName:
+            self.__DEFAULT_VALUES["Race"] = raceName
+            self.__SCHEMA_FLAT.insert(0, "Race")
 
     def getFiles(self):
         self.__logger.info("Searching for files in " + self.__inFoldName)
@@ -54,9 +60,9 @@ class Reshaper:
             self.__logger.info("All data written to " + self.__outFileName)
 
     def __writeHeader(self, outF):
-        outF.write(OUT_DELIM.join(SCHEMA_FLAT))
-        outF.write(OUT_DELIM)
-        outF.write(OUT_DELIM.join(SCHEMA_RESHAPE))
+        outF.write(self.__OUT_DELIM.join(self.__SCHEMA_FLAT))
+        outF.write(self.__OUT_DELIM)
+        outF.write(self.__OUT_DELIM.join(self.__SCHEMA_RESHAPE))
         outF.write("\n")
 
     def __isHeader(self, line):
@@ -66,7 +72,7 @@ class Reshaper:
         return not line.strip()
         
     def __getNumLaps(self, line):
-        return len(line.split(IN_DELIM)) - IGNORED_COLS - (S_FLAT_E-S_FLAT_S)
+        return len(line.split(self.__IN_DELIM)) - IGNORED_COLS - (S_FLAT_E-S_FLAT_S)
 
     def __isDNS(self, line):
         return line.startswith("-")
@@ -79,34 +85,31 @@ class Reshaper:
 
     def __getFlatRow(self, schema_map):
         row = []
-        for col in SCHEMA_FLAT:
+        for col in self.__SCHEMA_FLAT:
             row.append(schema_map[col])
-        return OUT_DELIM.join(row)
+        return self.__OUT_DELIM.join(row)
 
     def __writeReshapedData(self, outF, flatRow, schema_map):
         lap = 1
         col = Template("Lap $lap").substitute(lap=lap)
         while col in schema_map:
             outF.write(flatRow)
-            outF.write(OUT_DELIM)
-            outF.write(OUT_DELIM.join([str(lap), schema_map[col]]))
+            outF.write(self.__OUT_DELIM)
+            outF.write(self.__OUT_DELIM.join([str(lap), schema_map[col]]))
             outF.write("\n")
         
             lap += 1
             col = Template("Lap $lap").substitute(lap=lap)
 
     def __processFile(self, file, outF):
-        default_values ={}
-        for col in SCHEMA_FLAT:
-            default_values[col] = DEFAULT_VALUE
-        default_values["Category"] = DEFAULT_CATEGORY
-        schema = SCHEMA_FLAT
+        default_values = self.__DEFAULT_VALUES.copy()
+        schema = self.__SCHEMA_FLAT
 
         num_laps = -1
         with open(file, "r") as inF:
             for line in inF:
                 if self.__isHeader(line):
-                    schema = line.split(IN_DELIM)
+                    schema = line.split(self.__IN_DELIM)
                     continue
                 if self.__isBlank(line):
                     continue
@@ -117,7 +120,7 @@ class Reshaper:
                     continue
 
                 # else it's a racer with a place
-                vars = line.split(IN_DELIM)
+                vars = line.split(self.__IN_DELIM)
                 schema_map = default_values.copy()
                 for i in range(len(schema)):
                     if "Place" in schema[i]: # Weird case where this gets extra noise in the Cat 1/2 Men single file
@@ -140,6 +143,7 @@ if __name__ == "__main__":
     TXT file"')
     parser.add_argument('outFileName', metavar='outputFile', help="Path/name of the csv file to be created\
     for feeding into Tableau")
+    parser.add_argument('-r', dest="raceName", help="Name of the race")
     meg = parser.add_mutually_exclusive_group()
     meg.add_argument("-v", action="store_true", help="Print verbose information about program process")
     args = parser.parse_args()
@@ -147,6 +151,6 @@ if __name__ == "__main__":
     if args.v:
         logger.setLevel(logging.DEBUG)
 
-    reshaper = Reshaper(args.inFoldName, args.outFileName)
+    reshaper = Reshaper(args.inFoldName, args.outFileName, args.raceName)
     reshaper.getFiles()
     reshaper.outputData()
